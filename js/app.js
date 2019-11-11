9caa68ff609bd73eb3faef6603ff8f062496f27e
ameroran <oran.amer@yahoo.com>
2019-11-11 15:35:27 +0200

    add files

---
+++ js/app.js
@@ -0,0 +1,52 @@
+'use strict'
+
+var name= prompt('Hello. Whats your name?');
+alert( name+ ', Welcome to my page' );
+
+ var I = prompt("Am I love car?");
+ if (I === 'yes') {
+    alert('you have a point');
+    } else if(I=== 'no') {
+        alert('you don\'t take a point');
+    } 
+   // console.log(I);
+
+ var II = prompt("Am I have a drive license?");
+ if (II=== 'no') {
+    alert('you have a point');
+    } else {
+        alert('you don\'t take a point');
+    }
+   // console.log(II);
+    
+ var III = prompt("Am I love reading?");
+ if (III=== 'yes') {
+   alert('you have another point')
+    }else {
+        alert('you don\'t take a point');
+    } 
+  //  console.log(III);
+
+ var VI = prompt("Am I have IPHONE?");
+ if (VI=== 'no') {
+    alert('you have another point')
+    } else {
+        alert('you don\'t take a point');
+    } 
+// console.log(VI);
+
+ var V = prompt("Am I lived in AMMAN?");
+ if (V=== 'yes') {
+alert('good another point')    
+    } else {
+        alert('you don\'t take a point');
+    }
+    // console.log(V);
+ var answer ;
+ I = I.toLowerCase();
+II = II.toLowerCase();
+III= III.toLowerCase();
+VI = VI.toLowerCase();
+V = V.toLowerCase();
+        
+        
