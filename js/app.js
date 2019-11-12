93134463f20e47860b7219eff30a736ed2455856
ameroran <oran.amer@yahoo.com>
2019-11-12 11:29:18 +0200

    add text

--- js/app.js
+++ js/app.js
@@ -3,50 +3,51 @@
 var name= prompt('Hello. Whats your name?');
 alert( name+ ', Welcome to my page' );
 
- var I = prompt("Am I love car?");
- if (I === 'yes') {
+ var one = prompt("Am I love car?");
+ if (one === 'yes') {
     alert('you have a point');
     } else if(I=== 'no') {
         alert('you don\'t take a point');
     } 
    // console.log(I);
 
- var II = prompt("Am I have a drive license?");
- if (II=== 'no') {
+ var two = prompt("Am I have a drive license?");
+ if (two=== 'no') {
     alert('you have a point');
     } else {
         alert('you don\'t take a point');
     }
    // console.log(II);
     
- var III = prompt("Am I love reading?");
- if (III=== 'yes') {
+ var three = prompt("Am I love reading?");
+ if (three=== 'yes') {
    alert('you have another point')
     }else {
         alert('you don\'t take a point');
     } 
   //  console.log(III);
 
- var VI = prompt("Am I have IPHONE?");
- if (VI=== 'no') {
+ var four = prompt("Am I have IPHONE?");
+ if (four=== 'no') {
     alert('you have another point')
     } else {
         alert('you don\'t take a point');
     } 
 // console.log(VI);
 
- var V = prompt("Am I lived in AMMAN?");
- if (V=== 'yes') {
+ var five = prompt("Am I lived in AMMAN?");
+ if (five=== 'yes') {
 alert('good another point')    
     } else {
         alert('you don\'t take a point');
     }
     // console.log(V);
+alert( name+ ', Have fun');
+    
+ one = one.toLowerCase();
+ two = two.toLowerCase();
+ three= three.toLowerCase();
+ four = four.toLowerCase();
+ five = five.toLowerCase();
+        
  var answer ;
- I = I.toLowerCase();
-II = II.toLowerCase();
-III= III.toLowerCase();
-VI = VI.toLowerCase();
-V = V.toLowerCase();
-        
-        
